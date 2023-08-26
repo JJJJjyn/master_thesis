33ee9f4ebda73f57844f2408c9b4f5eacb88c741
YueningJia <93624809+JJJJjyn@users.noreply.github.com>
2023-08-26 23:31:50 +0800

    Add files via upload

---
+++ models/layers.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+from torch.utils.data import DataLoader, TensorDataset
+import utils.utils
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import cv2
+from task.loss import HeatmapLoss
+
+
+class CVAE(nn.Module):
+    """Implementation of CVAE(Conditional Variational Auto-Encoder)"""
+    def __init__(self, image_size, label_size, latent_size):
+        super(CVAE, self).__init__()
+
+        self.fc1 = nn.Linear(image_size * image_size + label_size, 200)
+        self.fc2_mu = nn.Linear(200, latent_size)
+        self.fc2_log_std = nn.Linear(200, latent_size)
+        self.fc3 = nn.Linear(286, 200)
+        self.fc4 = nn.Linear(200, image_size * image_size)
+
+    def encode(self, x, y):
+        h1 = F.relu(self.fc1(torch.cat([x, y], dim=1)))  # concat features and labels
+        mu = self.fc2_mu(h1)
+        log_std = self.fc2_log_std(h1)
+        return mu, log_std
+
+    def decode(self, z, y):
+        h3 = F.relu(self.fc3(torch.cat([z, y], dim=1)))  # concat latents and labels
+        recon = torch.sigmoid(self.fc4(h3))  # use sigmoid because the input image's pixel is between 0-1
+        return recon
+
+    def reparametrize(self, mu, log_std):
+        std = torch.exp(log_std)
+        eps = torch.randn_like(std)  # simple from standard normal distribution
+        z = mu + eps * std
+        return z
+
+    def forward(self, x, y):
+        mu, log_std = self.encode(x, y)
+        z = self.reparametrize(mu, log_std)
+        recon = self.decode(z, y)
+        return recon, mu, log_std
+
+    def loss_function(self, recon, x, mu, log_std) -> torch.Tensor:
+        recon_loss = F.mse_loss(recon, x, reduction="sum")  # use "mean" may have a bad effect on gradients
+        kl_loss = -0.5 * (1 + 2*log_std - mu.pow(2) - torch.exp(2*log_std))
+        kl_loss = torch.sum(kl_loss)
+        loss = recon_loss + kl_loss
+        return loss
+
+Pool = nn.MaxPool2d
+
+def batchnorm(x):
+    return nn.BatchNorm2d(x.size()[1])(x)
+
+class Conv(nn.Module):
+    def __init__(self, inp_dim, out_dim, kernel_size=3, stride = 1, bn = False, relu = True):
+        super(Conv, self).__init__()
+        self.inp_dim = inp_dim
+        self.conv = nn.Conv2d(inp_dim, out_dim, kernel_size, stride, padding=(kernel_size-1)//2, bias=True)
+        self.relu = None
+        self.bn = None
+        if relu:
+            self.relu = nn.ReLU()
+        if bn:
+            self.bn = nn.BatchNorm2d(out_dim)
+
+    def forward(self, x):
+        assert x.size()[1] == self.inp_dim, "{} {}".format(x.size()[1], self.inp_dim)
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+class Residual(nn.Module):
+    def __init__(self, inp_dim, out_dim):
+        super(Residual, self).__init__()
+        self.relu = nn.ReLU()
+        self.bn1 = nn.BatchNorm2d(inp_dim)
+        self.conv1 = Conv(inp_dim, int(out_dim/2), 1, relu=False)
+        self.bn2 = nn.BatchNorm2d(int(out_dim/2))
+        self.conv2 = Conv(int(out_dim/2), int(out_dim/2), 3, relu=False)
+        self.bn3 = nn.BatchNorm2d(int(out_dim/2))
+        self.conv3 = Conv(int(out_dim/2), out_dim, 1, relu=False)
+        self.skip_layer = Conv(inp_dim, out_dim, 1, relu=False)
+        if inp_dim == out_dim:
+            self.need_skip = False
+        else:
+            self.need_skip = True
+        
+    def forward(self, x):
+        if self.need_skip:
+            residual = self.skip_layer(x)
+        else:
+            residual = x
+        out = self.bn1(x)
+        out = self.relu(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out += residual
+        return out 
+
+class Hourglass(nn.Module):
+    def __init__(self, n, f, bn=None, increase=0):
+        super(Hourglass, self).__init__()
+        nf = f + increase
+        self.up1 = Residual(f, f)
+        # Lower branch
+        self.pool1 = Pool(2, 2)
+        self.low1 = Residual(f, nf)
+        self.n = n
+        # Recursive hourglass
+        if self.n > 1:
+            self.low2 = Hourglass(n-1, nf, bn=bn)
+        else:
+            self.low2 = Residual(nf, nf)
+        self.low3 = Residual(nf, f)
+        self.up2 = nn.Upsample(scale_factor=2, mode='nearest')
+
+    def forward(self, x):
+        up1  = self.up1(x)
+        pool1 = self.pool1(x)
+        low1 = self.low1(pool1)
+        low2 = self.low2(low1)
+        low3 = self.low3(low2)
+        up2  = self.up2(low3)
+        return up1 + up2
